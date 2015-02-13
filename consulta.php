7d9565f8489380c4a475fc31bf2ac8e8987b980e
Juan Mora <raicerk@gmail.com>
2015-02-13 17:46:22 -0300

    Se modifica consulta de inversiones, ademas se cambia nombre a archivo a cargo de la carga dinamica de los datos en la ddbb

--- consulta.php
+++ consulta.php
@@ -5,6 +5,37 @@ $funciones = new Funciones();
 
 $resultado = $funciones->query("SELECT * FROM mutuos WHERE fondo IN ('BTG PACTUAL ACCIONES ASIA EMERGENTE B','PRINCIPAL USA G')",'',false);
 
-print_r($resultado);
+echo "<table border='1'>";
+echo "<tr>";
+	echo "<td>fondo</td>";
+	echo "<td>administradora</td>";
+	echo "<td>valor</td>";
+	echo "<td>porcentaje_variacion_dia</td>";
+	echo "<td>porcentaje_nominal_treita_dias</td>";
+	echo "<td>porcentaje_real_UF_treinta_dias</td>";
+	echo "<td>porcentaje_nominal_tres_meses</td>";
+	echo "<td>porcentaje_real_UF_tres_meses</td>";
+	echo "<td>porcentaje_nominal_un_ano</td>";
+	echo "<td>porcentaje_real_UF_un_ano</td>";
+	echo "<td>fecha</td>";
+	echo "<td>hora</td>";
+	echo "</tr>";
+foreach ($resultado as $result) {
+	echo "<tr>";
+	echo "<td>".$result['fondo']."</td>";
+	echo "<td>".$result['administradora']."</td>";
+	echo "<td>".str_replace(".", ",", $result['valor'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_variacion_dia'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_nominal_treita_dias'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_real_UF_treinta_dias'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_nominal_tres_meses'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_real_UF_tres_meses'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_nominal_un_ano'])."</td>";
+	echo "<td>".str_replace(".", ",", $result['porcentaje_real_UF_un_ano'])."</td>";
+	echo "<td>".$result['fecha']."</td>";
+	echo "<td>".$result['hora']."</td>";
+	echo "</tr>";
 
+}
+echo "</table>";
 ?>
